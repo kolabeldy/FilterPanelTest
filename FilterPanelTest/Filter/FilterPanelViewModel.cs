27f02dd2512712a258049a9f617cedf2e3cb8ab7
Kazimir <avvlasov@outlook.com>
2021-11-12 12:51:49 +0300

    вынес struct FilterSet в отдельный файл

--- FilterPanelTest/Filter/FilterPanelViewModel.cs
+++ FilterPanelTest/Filter/FilterPanelViewModel.cs
@@ -3,21 +3,6 @@
 namespace FilterPanelTest.Filter;
 public delegate void IsFilterChanged(FilterSet filterSet);
 public delegate void IsFilterPanelClosed();
-public struct FilterSet
-{
-    public int StartPeriod { get; set; }
-    public DateTime StartDate { get; set; }
-    public int EndPeriod { get; set; }
-    public DateTime EndDate { get; set; }
-    public int StartDynamicPeriod { get; set; }
-    public int EndDynamicPeriod { get; set; }
-    public List<CostCenter> SelectedCC { get; set; }
-    public List<EnergyResource> SelectedER { get; set; }
-    public int SelectedNormTypes { get; set; }
-    public string TitleSelectedCCGroup { get; set; }
-    public string TitleSelectedERGroup { get; set; }
-    public string TitleSelectedIsNormGroup { get; set; }
-}
 
 public class FilterPanelViewModel
 {
---
+++ FilterPanelTest/Filter/FilterSet.cs
@@ -0,0 +1,16 @@
+﻿namespace FilterPanelTest.Filter;
+public struct FilterSet
+{
+    public int StartPeriod { get; set; }
+    public DateTime StartDate { get; set; }
+    public int EndPeriod { get; set; }
+    public DateTime EndDate { get; set; }
+    public int StartDynamicPeriod { get; set; }
+    public int EndDynamicPeriod { get; set; }
+    public List<CostCenter> SelectedCC { get; set; }
+    public List<EnergyResource> SelectedER { get; set; }
+    public int SelectedNormTypes { get; set; }
+    public string TitleSelectedCCGroup { get; set; }
+    public string TitleSelectedERGroup { get; set; }
+    public string TitleSelectedIsNormGroup { get; set; }
+}
