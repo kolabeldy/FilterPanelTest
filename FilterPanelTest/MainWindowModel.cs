851b16594414334f9456d34441a3aa361fbbc48f
Власов Андрей <avvlasov@outlook.com>
2021-11-17 11:25:33 +0300

    Очистил от старого формама фильтра

--- FilterPanelTest/MainWindowModel.cs
+++ FilterPanelTest/MainWindowModel.cs
@@ -16,13 +16,6 @@ public class MainWindowModel : BaseViewModel
 
     public bool FiltersIsChanged;
 
-
-
-
-    public FilterPanel FilterPanel { get; set; }
-
-    protected FilterPanelViewModel filterPanelViewModel;
-
     protected bool _IsFilterPopupOpen = false;
     public bool IsFilterPopupOpen
     {
@@ -50,116 +43,42 @@ public class MainWindowModel : BaseViewModel
         set
         {
             Set(ref _FilterSet, value);
-            TitleStartPeriod = value.StartPeriod;
-            TitleEndPeriod = value.EndPeriod;
-            TitleStartDynamicPeriod = value.StartDynamicPeriod;
-            TitleEndDynamicPeriod = value.EndDynamicPeriod;
-
-            TitleSelectedCCGroup = value.TitleSelectedCCGroup;
-            TitleSelectedERGroup = value.TitleSelectedERGroup;
-        }
-    }
-    private int _TitleStartPeriod;
-    public int TitleStartPeriod
-    {
-        get => _TitleStartPeriod;
-        set
-        {
-            Set(ref _TitleStartPeriod, value);
-        }
-    }
-    private int _TitleEndPeriod;
-    public int TitleEndPeriod
-    {
-        get => _TitleEndPeriod;
-        set
-        {
-            Set(ref _TitleEndPeriod, value);
-        }
-    }
-    private int _TitleStartDynamicPeriod;
-    public int TitleStartDynamicPeriod
-    {
-        get => _TitleStartDynamicPeriod;
-        set
-        {
-            Set(ref _TitleStartDynamicPeriod, value);
-        }
-    }
-    private int _TitleEndDynamicPeriod;
-    public int TitleEndDynamicPeriod
-    {
-        get => _TitleEndDynamicPeriod;
-        set
-        {
-            Set(ref _TitleEndDynamicPeriod, value);
-        }
-    }
-
-    private string _TitleSelectedCCGroup;
-    public string TitleSelectedCCGroup
-    {
-        get => _TitleSelectedCCGroup;
-        set
-        {
-            Set(ref _TitleSelectedCCGroup, value);
-        }
-    }
-    private string _TitleSelectedERGroup;
-    public string TitleSelectedERGroup
-    {
-        get => _TitleSelectedERGroup;
-        set
-        {
-            Set(ref _TitleSelectedERGroup, value);
         }
     }
 
 
-    private List<FilterTable> GetFilters(FilterSet filter)
-    {
-        List<FilterTable> result = new();
-        result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodMin", Value = FilterSet.StartPeriod });
-        result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodMax", Value = FilterSet.EndPeriod });
-        result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodDynamicMin", Value = FilterSet.StartDynamicPeriod });
-        result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodDynamicMax", Value = FilterSet.EndDynamicPeriod });
-        foreach (var r in FilterSet.SelectedCC)
-        {
-            result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "CC", Value = r.Id });
-        }
-        foreach (var r in FilterSet.SelectedER)
-        {
-            result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "ER", Value = r.Id });
-        }
+    //private List<FilterTable> GetFilters(FilterSet filter)
+    //{
+    //    List<FilterTable> result = new();
+    //    result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodMin", Value = FilterSet.StartPeriod });
+    //    result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodMax", Value = FilterSet.EndPeriod });
+    //    result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodDynamicMin", Value = FilterSet.StartDynamicPeriod });
+    //    result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "PeriodDynamicMax", Value = FilterSet.EndDynamicPeriod });
+    //    foreach (var r in FilterSet.SelectedCC)
+    //    {
+    //        result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "CC", Value = r.Id });
+    //    }
+    //    foreach (var r in FilterSet.SelectedER)
+    //    {
+    //        result.Add(new FilterTable { Category = "Analysis", Item = "Use", Indicator = "ER", Value = r.Id });
+    //    }
 
-        return result;
-    }
-    protected void Refresh(FilterSet filterSet)
-    {
-        this.FilterSet = filterSet;
-        FilterTable filterTable = new();
-        filterTable.Delete("Analysis", "Use");
-        filterTable.AddRange(GetFilters(filterSet));
-    }
+    //    return result;
+    //}
+    //protected void Refresh(FilterSet filterSet)
+    //{
+    //    this.FilterSet = filterSet;
+    //    FilterTable filterTable = new();
+    //    filterTable.Delete("Analysis", "Use");
+    //    filterTable.AddRange(GetFilters(filterSet));
+    //}
 
 
     public MainWindowModel(bool periodVisible, bool ccVisible, bool erVisible, bool ntVisible)
     {
-        OldFilterInit(periodVisible, ccVisible, erVisible, ntVisible);
         NewFilterInit();
     }
 
-    private void OldFilterInit(bool periodVisible, bool ccVisible, bool erVisible, bool ntVisible)
-    {
-        FilterSet filterSet = new();
-        FilterSet = new();
-        filterPanelViewModel = new FilterPanelViewModel(ref filterSet, periodVisible, ccVisible, erVisible, ntVisible);
-        FilterSet = filterSet;
-        filterPanelViewModel.OnFilterPanelClosed += FilterClose;
-        filterPanelViewModel.OnFilterChanged += Refresh;
-        FilterPanel = new FilterPanel(filterPanelViewModel);
-        Refresh(FilterSet);
-    }
     private void NewFilterInit()
     {
         modelDate = new FilterSectionViewModel();
